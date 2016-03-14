760dfea60032538ccc2278f4052e573f28c71d21
DAAuerbach <auerbach.dan@gmail.com>
2016-03-14 13:03:02 -0400

    add getStreamCatST function, amend readme

--- README.md
+++ README.md
@@ -13,6 +13,11 @@ This repo contains working scripts for accessing and manipulating StreamCat data
 
 Scripts depend on an updated base R installation as well as various packages. Some effort has been made to ensure that dependencies are installed and loaded, but this is firmly "research grade" scripting - **_CAVEAT EMPTOR!_**
 
+##Functions
+
+ + [scu\_getStreamCatST](https://github.com/daauerbach/streamcatUtils/blob/master/scu_getStreamCatST.R) takes a 2-character (caps) state abbreviation and returns a tbl_df dataframe of n-obs COMIDs with n-features ("attributes" or columns) using data.table::fread. It currently scrapes all available data per state but could be easily modified to return selected features/columns. Run time is mostly downloads. Objects can optionally be retained as .rds, which can provide substantial storage benefits over .csv (which are also easily written out).
+
+
 
 ## Disclaimer
 This code is provided on an "as is" basis and the user assumes responsibility for its use.  Any reference to specific commercial products, processes, or services by service mark, trademark, manufacturer, or otherwise, does not constitute or imply their endorsement, recommendation or favoring.
---
+++ scu_getStreamCatST.R
@@ -0,0 +1,50 @@
+# Takes a 2-character (caps) state abbreviation, returns a tbl_df dataframe of n-obs COMIDs with n-features
+# Revised to dplyr from original data.table version, maintaining fast data.table merge
+# Run time mostly downloads
+
+# Subsets to state of interest from a vector of attribute .csv file names,
+# then builds a list of csv data; note COMID ordering over rows differs across tables, but all appear equivalently present 
+# Drops some identical/redundant columns and reshapes
+
+#KY, SD, NE failed on a few duplicate row names/COMID, now tests and removes
+#could add various other fussy bits: check for correct capitalization, allow full state names, etc.
+
+getStreamCatST = function(state, #2 character abbreviation
+                          repos = "ftp://newftp.epa.gov/EPADataCommons/ORD/NHDPlusLandscapeAttributes/StreamCat/States/",
+                          dirOut = NULL,
+                          rds = T
+){
+   if (!require("RCurl")) { install.packages("RCurl", dependencies = TRUE); library(RCurl)}
+   if (!require("data.table")) { install.packages("data.table", dependencies = TRUE) } #; library(data.table)
+   if (!require("dplyr")) { install.packages("dplyr", dependencies = TRUE); library(dplyr)}
+   
+   attribfn = sub("\r", "", strsplit(getURL(repos, dirlistonly = TRUE), "\n")[[1]])
+   attribfnst = paste0(repos,grep(paste0("_",state), attribfn, value=T))
+
+   stsc1 = data.table::fread(attribfnst[1], data.table = T)  #no stringsAsFactors = F since no strings
+   setkey(stsc1, COMID)
+   stsc = Reduce(function(x,y) merge(x, y, by="COMID"),
+                 lapply(
+                    lapply(attribfnst[2:length(attribfnst)]
+                           ,data.table::fread
+                           ,drop=c("CatAreaSqKm","CatPctFull","WsAreaSqKm","WsPctFull")
+                           ,data.table = T)
+                    ,setkey, COMID)
+                  ) #end Reduce
+   stsc = tbl_df(merge(stsc1, stsc, by="COMID"))
+   
+   if(anyDuplicated(stsc)>0) {
+      print("Duplicate COMID ID rows removed from data.table")
+      stsc = distinct(stsc)
+      }
+
+   row.names(stsc) = as.character(stsc$COMID)
+   if(!is.null(dirOut)) {
+      if(rds) {
+         saveRDS(stsc, file=paste0(dirOut,"/catdata",state,".rds")) } else {
+            write.csv(stsc, file=paste0(dirOut,"/catdata",state,".csv"), row.names = F)
+         }
+      }
+      
+   return(stsc)
+} #end getStreamCat
